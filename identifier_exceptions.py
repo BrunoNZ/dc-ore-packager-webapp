5799989bc4b31a92cac70fea093a459260787194
Bruno Nocera Zanette <brunonzanette@gmail.com>
2020-08-02 00:05:32 -0300

    update Identifier_Exceptions

--- identifier_exceptions.py
+++ identifier_exceptions.py
@@ -1,6 +1,7 @@
 idExceptions = {
-    "http://repositorio.unicamp.br":"localhost",
-    "https://tede.ufam.edu.br":"tede.ufam.edu.br",
-    "https://app.uff.br":"app.uff.br",
-    "http://ri.ufmt.br":"localhost",
+    "http://repositorio.unicamp.br":{"id": "localhost"},
+    "https://tede.ufam.edu.br":{"id": "tede.ufam.edu.br"},
+    "https://app.uff.br":{"id": "app.uff.br"},
+    "http://ri.ufmt.br":{"id": "localhost"},
+    "http://repositorio.bc.ufg.br":{"useIdPrefix": True}
 }
